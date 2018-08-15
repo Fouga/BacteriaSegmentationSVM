8432ba9a4f1a9f38a8fe9aca7b79880382be657d
Fouga <natalia.chicherova@gmail.com>
2018-08-15 17:04:16 +0200

    take into account txt load when not all the slice are stitched

--- functions/putAlltxtTogether.m
+++ functions/putAlltxtTogether.m
@@ -1,7 +1,7 @@
 function putAlltxtTogether(options)
 
 B = [];cnt = 1;% cnt is for verstical axis
-for i = 1:options.number_of_frames*options.number_of_optic_sec
+for i = 1:size(options.ALLfilenames,1)
   % load images
     txt_name = fullfile(options.folder_destination_perSlice, ['positions_', options.ALLfilenames{i},  '.txt']);    
     A = readtable(txt_name);
